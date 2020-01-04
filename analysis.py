beb4b1d0e93008bb75f1872956785e390f132025
watate <walter.spades@gmail.com>
2020-01-05 00:57:31 +0800

    analysis2 is now a preprocess file

--- analysis.py
+++ analysis.py
@@ -2,122 +2,127 @@ import pickle
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
+import seaborn as sb
 import pandas as pd
-sns.set(style="darkgrid")
-
-velocity_list_filename = 'velocity_list.dat'
-
-if os.path.exists(velocity_list_filename):
-    with open(velocity_list_filename, 'rb') as rfp:
-        velocity_list = pickle.load(rfp)
-
-linear_velocity = list()
-angular_velocity = list()
-linear_acceleration = list()
-linear_jerk = list()
-#noise = list()
-#difference = list()
-
-starting = 30
-j = 0
-
-for i in velocity_list:
-	if j > starting:
-		linear_velocity.append(i[0])
-		angular_velocity.append(i[1])
-		#difference.append(i[0] - i[2])
-	j = j + 1
-
-previous_vel = 0
-current_vel = 0
-timestep = 1
-
-#calculate acceleration
-	#acceleration here is timestep derivative of velocity
-for i in linear_velocity:
-	current_vel = i
-	linear_acceleration.append((current_vel - previous_vel)/timestep)
-	previous_vel = current_vel
-
-#calculate jerk
-previous_accel = 0
-current_accel = 0
-timestep = 1
-
-#calculate acceleration
-	#acceleration here is timestep derivative of velocity
-for i in linear_acceleration:
-	current_accel = i
-	linear_jerk.append((current_accel - previous_accel)/timestep)
-	previous_accel = current_accel
-
 
 ############## VISUALIZATION ##################
+#Initial settings
+sb.set(style="darkgrid")
+sb.set_context("paper") #other formats: "talk", "poster"
 
+#Loading the DataFrame generated from preprocess (analysis2.py)
+if os.path.exists('analysis_dataframe.dat'):
+    with open('analysis_dataframe.dat', 'rb') as rfp:
+        df = pickle.load(rfp)
+
+# #Generating the DataFrame
 # df = pd.DataFrame(dict(time=np.arange(len(linear_velocity)),
 #                        linear_velocity=linear_velocity,
 # 						linear_acceleration=linear_acceleration,
-# 						linear_jerk=linear_jerk))
-# g = sns.relplot(x="time", y="linear_velocity", kind="line", data=df)
-# g.fig.autofmt_xdate()
+# 						linear_jerk=linear_jerk,
+# 						angular_velocity=angular_velocity,
+# 						angular_acceleration=angular_acceleration,
+# 						angular_jerk=angular_jerk,
+# 						))
 
-
-#plot linear velocity
-x = np.linspace(1, len(linear_velocity), len(linear_velocity))
-plt.plot(x, linear_velocity, label="Linear Velocity")
-plt.xlabel('Timestep')
-plt.ylabel('Velocity')
-plt.title('Velocity vs Timestep')
-plt.legend()
-plt.grid()
+######### ANGULAR ############
+#Generating histograms
+g = sb.distplot(df["angular_velocity"], kde = False)
 plt.show()
 
-#plot linear acceleration
-x = np.linspace(1, len(linear_acceleration), len(linear_acceleration))
-plt.plot(x, linear_acceleration, label="Linear Acceleration")
-plt.xlabel('Timestep')
-plt.ylabel('Acceleration')
-plt.title('Acceleration vs Timestep')
-plt.legend()
-plt.grid()
+g = sb.distplot(df["angular_jerk"], kde = False)
 plt.show()
 
-#plot linear jerk
-x = np.linspace(1, len(linear_jerk), len(linear_jerk))
-plt.plot(x, linear_jerk, label="Linear Jerk")
-plt.xlabel('Timestep')
-plt.ylabel('Jerk')
-plt.title('Jerk vs Timestep')
-plt.legend()
-plt.grid()
+#Generating the graphs
+g = sb.relplot(x="time", y="angular_velocity", kind="line", data=df)
+g.fig.autofmt_xdate()
 plt.show()
 
-#plot linear jerk
-x = np.linspace(1, len(linear_jerk), len(linear_jerk))
-plt.plot(x, linear_velocity, label="Linear Velocity")
-plt.plot(x, linear_acceleration, label="Linear Acceleration")
-plt.plot(x, linear_jerk, label="Linear Jerk")
-plt.xlabel('Timestep')
-plt.ylabel('Variable')
-plt.title('Variable vs Timestep')
-plt.legend()
+g = sb.relplot(x="time", y="angular_acceleration", kind="line", data=df)
+g.fig.autofmt_xdate()
+plt.show()
+
+g = sb.relplot(x="time", y="angular_jerk", kind="line", data=df)
+g.fig.autofmt_xdate()
+plt.show()
+
+########### LINEAR #############
+#Generating histograms
+g = sb.distplot(df["linear_velocity"], kde = False)
+plt.show()
+
+g = sb.distplot(df["linear_jerk"], kde = False)
+plt.show()
+
+#Generating the graphs
+g = sb.relplot(x="time", y="linear_velocity", kind="line", data=df)
+g.fig.autofmt_xdate()
+plt.show()
+
+g = sb.relplot(x="time", y="linear_acceleration", kind="line", data=df)
+g.fig.autofmt_xdate()
+plt.show()
+
+g = sb.relplot(x="time", y="linear_jerk", kind="line", data=df)
+g.fig.autofmt_xdate()
 plt.show()
 
 
-#plot linear velocity and angular velocity
-x = np.linspace(1, len(linear_velocity), len(linear_velocity))
-plt.plot(x, linear_velocity, label="Linear Velocity")
-plt.plot(x, angular_velocity, label="Angular Velocity")
-plt.xlabel('Timestep')
-plt.ylabel('Velocity')
-plt.title('Velocity vs Timestep')
-plt.legend()
-plt.show()
+# #plot linear velocity
+# x = np.linspace(1, len(linear_velocity), len(linear_velocity))
+# plt.plot(x, linear_velocity, label="Linear Velocity")
+# plt.xlabel('Timestep')
+# plt.ylabel('Velocity')
+# plt.title('Velocity vs Timestep')
+# plt.legend()
+# plt.grid()
+# plt.show()
 
-x = np.linspace(1, len(angular_velocity), len(angular_velocity))
-plt.plot(x, angular_velocity)
-plt.xlabel('Timestep')
-plt.ylabel('Angular Velocity')
-plt.title('Angular Velocity vs Timestep')
-plt.show()
+# #plot linear acceleration
+# x = np.linspace(1, len(linear_acceleration), len(linear_acceleration))
+# plt.plot(x, linear_acceleration, label="Linear Acceleration")
+# plt.xlabel('Timestep')
+# plt.ylabel('Acceleration')
+# plt.title('Acceleration vs Timestep')
+# plt.legend()
+# plt.grid()
+# plt.show()
+
+# #plot linear jerk
+# x = np.linspace(1, len(linear_jerk), len(linear_jerk))
+# plt.plot(x, linear_jerk, label="Linear Jerk")
+# plt.xlabel('Timestep')
+# plt.ylabel('Jerk')
+# plt.title('Jerk vs Timestep')
+# plt.legend()
+# plt.grid()
+# plt.show()
+
+# #plot linear jerk
+# x = np.linspace(1, len(linear_jerk), len(linear_jerk))
+# plt.plot(x, linear_velocity, label="Linear Velocity")
+# plt.plot(x, linear_acceleration, label="Linear Acceleration")
+# plt.plot(x, linear_jerk, label="Linear Jerk")
+# plt.xlabel('Timestep')
+# plt.ylabel('Variable')
+# plt.title('Variable vs Timestep')
+# plt.legend()
+# plt.show()
+
+
+# #plot linear velocity and angular velocity
+# x = np.linspace(1, len(linear_velocity), len(linear_velocity))
+# plt.plot(x, linear_velocity, label="Linear Velocity")
+# plt.plot(x, angular_velocity, label="Angular Velocity")
+# plt.xlabel('Timestep')
+# plt.ylabel('Velocity')
+# plt.title('Velocity vs Timestep')
+# plt.legend()
+# plt.show()
+
+# x = np.linspace(1, len(angular_velocity), len(angular_velocity))
+# plt.plot(x, angular_velocity)
+# plt.xlabel('Timestep')
+# plt.ylabel('Angular Velocity')
+# plt.title('Angular Velocity vs Timestep')
+# plt.show()
--- analysis2.py
+++ analysis2.py
@@ -1,4 +1,93 @@
-import time
-x = time.strftime("%Y%m%d-%H%M%S")
+#### Data Preprocessing
 
-print(x)
+import pickle
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sb
+import pandas as pd
+
+velocity_list_filename = 'velocity_list.dat'
+
+if os.path.exists(velocity_list_filename):
+    with open(velocity_list_filename, 'rb') as rfp:
+        velocity_list = pickle.load(rfp)
+
+linear_velocity = list()
+angular_velocity = list()
+linear_acceleration = list()
+linear_jerk = list()
+angular_acceleration = list()
+angular_jerk = list()
+#noise = list()
+#difference = list()
+
+starting = 30
+j = 0
+
+for i in velocity_list:
+	if j > starting:
+		linear_velocity.append(i[0])
+		angular_velocity.append(i[1])
+		#difference.append(i[0] - i[2])
+	j = j + 1
+
+previous_vel = 0
+current_vel = 0
+timestep = 1
+
+#calculate acceleration
+	#acceleration here is timestep derivative of velocity
+for i in linear_velocity:
+	current_vel = i
+	linear_acceleration.append((current_vel - previous_vel)/timestep)
+	previous_vel = current_vel
+
+previous_vel = 0
+current_vel = 0
+timestep = 1
+
+#calculate acceleration
+	#acceleration here is timestep derivative of velocity
+for i in angular_velocity:
+	current_vel = i
+	angular_acceleration.append((current_vel - previous_vel)/timestep)
+	previous_vel = current_vel
+
+#calculate jerk
+previous_accel = 0
+current_accel = 0
+timestep = 1
+
+#calculate jerk
+	#jerk here is timestep derivative of acceleration
+for i in linear_acceleration:
+	current_accel = i
+	linear_jerk.append((current_accel - previous_accel)/timestep)
+	previous_accel = current_accel
+
+#calculate jerk
+previous_accel = 0
+current_accel = 0
+timestep = 1
+
+#calculate jerk
+	#jerk here is timestep derivative of acceleration
+for i in angular_acceleration:
+	current_accel = i
+	angular_jerk.append((current_accel - previous_accel)/timestep)
+	previous_accel = current_accel
+
+
+#Generating the DataFrame
+df = pd.DataFrame(dict(time=np.arange(len(linear_velocity)),
+                       linear_velocity=linear_velocity,
+						linear_acceleration=linear_acceleration,
+						linear_jerk=linear_jerk,
+						angular_velocity=angular_velocity,
+						angular_acceleration=angular_acceleration,
+						angular_jerk=angular_jerk,
+						))
+
+with open('analysis_dataframe.dat','wb') as wfp:
+	pickle.dump(df, wfp)
