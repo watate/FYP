cbfab6f6073983f8cbf3f2759302aea4bc2ef38d
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2020-02-29 17:38:05 +0800

    dataframe now generated from a proper dictionary (for better ylabel names)

--- analysis_create_tfsummary_graph.py
+++ analysis_create_tfsummary_graph.py
@@ -10,7 +10,9 @@ import seaborn as sb
 import pandas as pd
 
 #filename = 
-filename = "20200226-093050_3000b_dataframe.dat"
+#filename = "20200226-093050_3000b_dataframe.dat" #Blank World
+#filename = "20200226-170933_3000b16000s_dataframe.dat" #Simple World
+filename = "20200227-220051_3000b16000s30000c_dataframe.dat" #Complex World
 
 #prevent xlabel cutting off
 	#see: https://stackoverflow.com/questions/6774086/why-is-my-xlabel-cut-off-in-my-matplotlib-plot
@@ -74,15 +76,18 @@ groups = [steps[x:x+100] for x in range(0, len(steps), 100)]
 average_steps = [sum(group)/len(group) for group in groups]
 groups = [pid_rate[x:x+100] for x in range(0, len(pid_rate), 100)]
 average_pid_rate = [sum(group)/len(group) for group in groups]
+policy_usage = [1 - i for i in average_pid_rate]
+
+data = {"Time": np.arange(len(average_performance)),
+		"Average Reward": average_reward,
+		"Average Performance": average_performance,
+		"Average Distance": average_distance,
+		"Average Steps": average_steps,
+		"Average PID Rate": average_pid_rate,
+		"Policy Usage": policy_usage}
 
 #Generate second dataframe for averaged results
-df2 = pd.DataFrame(dict(time=np.arange(len(average_performance)),
-						average_reward=average_reward,
-						average_performance=average_performance,
-						average_distance=average_distance,
-						average_steps=average_steps,
-						average_pid_rate=average_pid_rate,
-						))
+df2 = pd.DataFrame(data, columns=["Time", "Average Reward", "Average Performance", "Average Distance", "Average Steps", "Average PID Rate", "Policy Usage"])
 ##############################################################################
 ##############################################################################
 #1. Data best represented on line graphs
@@ -91,10 +96,12 @@ sb.set_context("paper") #other formats: "talk", "poster"
 save_dir = "pictures/paper/"
 fig_dpi = 300
 
+line_plots = ["Time", "Average Reward", "Average Performance", "Average Distance", "Average Steps", "Average PID Rate", "Policy Usage"]
 #Generating line graphs
 for i in line_plots:
-	g = sb.relplot(x="time", y=i, kind="line", data=df2)
+	g = sb.relplot(x="Time", y=i, kind="line", data=df2)
 	#g.fig.autofmt_xdate()
+	g.set(xlabel='Time (x10^2 episodes)')
 	fig = g.fig
 	fig.savefig(save_dir + "paper_" + i + "_line" + ".png", dpi=fig_dpi)
 	plt.cla()
@@ -123,8 +130,9 @@ fig_dpi = 300
 
 #Generating line graphs
 for i in line_plots:
-	g = sb.relplot(x="time", y=i, kind="line", data=df2)
+	g = sb.relplot(x="Time", y=i, kind="line", data=df2)
 	#g.fig.autofmt_xdate()
+	g.set(xlabel='Time (x10^2 episodes)')
 	fig = g.fig
 	fig.savefig(save_dir + "paper_" + i + "_line" + ".png", dpi=fig_dpi)
 	plt.cla()
