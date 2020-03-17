c9ac5724336a45531c8e77caf3e298ba142e2f8c
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2020-03-17 21:48:56 +0800

    added more filenames

--- analysis_compare_tfsummary.py
+++ analysis_compare_tfsummary.py
@@ -4,8 +4,8 @@
 	#2. Group data together according to the variable (e.g. Reward, PIDRate, etc.)
 		#Each variable should get its own dataframe
 	#3. Save dataframes to dataframe folder with appropriate variable names
-
-#If more dataframes are added, locations where we need to add more code is indicated by #ADD_HERE
+#Script 2: This script is used after analysis_compare_tfsummary_EMA or analysis_tfsummary
+	#That is, we only use this script after we have the dataframes and we want to put them together in a combined dataframe
 
 import pickle
 import os
@@ -14,13 +14,23 @@ import numpy as np
 import seaborn as sb
 import pandas as pd
 
-##############################################################################
-#Prepare and choose datasets from which to generate graphs #ADD_HERE
-filenames = ["20200302-044536_24000s_dataframe.dat", 
-"20200311-004451_VSmooth_Simple_dataframe.dat", "20200309-000445_Jerk_Simple_dataframe.dat"] #Simple Worlds
 
-#filenames = ["Normal_Simple_average.dat", "VSmooth_Simple_average.dat", "Jerk_Simple_average.dat"] #Simple Averaged
-#filenames = ["dataframe_averages/" + i for i in filenames]
+#Prepare and choose datasets from which to generate graphs #ADD_HERE
+########################### SIMPLE #########################################
+#Simple Raw
+#filenames = ["20200302-044536_24000s_dataframe.dat", "20200311-004451_VSmooth_Simple_dataframe.dat", "20200309-000445_Jerk_Simple_dataframe.dat"]
+
+#Simple Smooth
+#filenames = ["Normal_Simple_smooth.dat", "VSmooth_Simple_smooth.dat", "Jerk_Simple_smooth.dat"]
+#filenames = ["dataframe_smoothed/" + i for i in filenames]
+
+########################### COMPLEX #########################################
+#Complex Raw
+#filenames = ["20200301-074856_34000c_dataframe.dat", "20200311-203436_VSmooth_Complex_dataframe.dat", "20200309-214742_Jerk_Complex_dataframe.dat"]
+
+#Complex Smooth
+filenames = ["Normal_Complex_smooth.dat", "VSmooth_Complex_smooth.dat", "Jerk_Complex_smooth.dat"]
+filenames = ["dataframe_smoothed/" + i for i in filenames]
 
 #Loading the DataFrame generated from preprocess (analysis_tf_summary.py)
 #ADD_HERE
@@ -45,18 +55,18 @@ df_list = [df0, df1, df2] #ADD_HERE
 # Extracting specific columns: 
 	#https://stackoverflow.com/questions/34682828/extracting-specific-selected-columns-to-new-dataframe-as-a-copy
 #ADD_HERE
-#df_qmax = df_list[0].filter(["time"], axis=1)
-df_qmax = df_list[0].filter(["Time"], axis=1) #For averaged dataframes
-df_qmax = df_qmax.assign(avp0=df0["reward"].copy())
-df_qmax = df_qmax.assign(avp1=df1["reward"].copy())
-df_qmax = df_qmax.assign(avp2=df2["reward"].copy())
+
+df_new = pd.DataFrame(df0.filter(["Time"], axis=1))
+df_new = df_new.assign(avp0=df0["Success Rate"].copy())
+df_new = df_new.assign(avp1=df1["Success Rate"].copy())
+df_new = df_new.assign(avp2=df2["Success Rate"].copy())
 
 #Rename columns again
-df_qmax.columns = ["Time", "Normal Model", "Model with Velocity Smoother", "Model with Jerk-Learning"] #ADD_HERE
+df_new.columns = ["Time", "Normal Model", "Model with Velocity Smoother", "Model with Jerk-Learning"] #ADD_HERE
 
 #Drop rows that have NaN (i.e. follow the dataframe with the least number of rows)
-df_qmax = df_qmax.dropna().copy()
+df_new = df_new.dropna().copy()
 
 ######################## DATA PREPROCESSING (PD.MELT) #########################
-with open('dataframes/simple_reward_3models.dat','wb') as wfp:
-	pickle.dump(df_qmax, wfp)
+with open('dataframes/complex_success_rate_smooth_3models.dat','wb') as wfp:
+	pickle.dump(df_new, wfp)
